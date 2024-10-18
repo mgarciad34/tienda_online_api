b79b82e15b242e735848faca5f71560b2582be1a
Manuel Garcia <m.gd@outlook.es>
2024-10-18 23:02:26 +0200

    Creando crud categorias

---
+++ app/Http/Controllers/AdminCategoriasController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+use Illuminate\Support\Facades\Validator;
+
+class AdminCategoriasController extends Controller
+{
+    public function crearCategoria(Request $request){
+        $validatedData = $request->validate([
+            'nombre' => 'required|string|max:255',
+        ]);
+
+        try{
+            $categoria = Categoria::create($validatedData);
+            return response()->json(['message' => 'Categoria creada', 'categoria' => $categoria], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error al crear la categoria: ' . $e->getMessage()], 422);
+        }
+    }
+
+    public function obtenerCategorias(Request $request)
+    {
+        try {
+            $categorias = Categoria::all();
+
+            return response()->json([
+                'mensaje' => 'Categorías obtenidas',
+                'categorias' => $categorias,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Ocurrió un error al obtener las categorías: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function obtenerCategoriaId($id)
+    {
+        try {
+            $categoria = Categoria::findOrFail($id);
+
+            return response()->json([
+                'mensaje' => 'Categoria obtenida con éxito',
+                'categoria' => $categoria,
+            ], 200);
+        } catch (\ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'No se encontró la categoría con el ID especificado',
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Ocurrió un error al obtener la categoría: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function actualizarCategoria(Request $request, $id)
+    {
+        try {
+            $validatedData = $request->validate([
+                'nombre' => 'required|string|max:255',
+            ]);
+
+            $categoria = Categoria::findOrFail($id);
+
+            $categoria->update($validatedData);
+
+            return response()->json([
+                'mensaje' => 'Categoria actualizada con éxito',
+                'categoria' => $categoria,
+            ], 200);
+        } catch (\ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'No se encontró la categoría con el ID especificado',
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Ocurrió un error al actualizar la categoría: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+}
