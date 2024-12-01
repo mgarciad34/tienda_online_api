5575de446009cc40cfbbcbfc6052da6b101c8096
Manuel Garcia <m.gd@outlook.es>
2024-12-01 18:44:55 +0100

    arreglamos funcionalidad pasarela de pago cierro cesta y abro otra nueva

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,17 +8,42 @@ use Stripe\PaymentIntent;
 
 class PaymentController extends Controller
 {
+    public function gestionCestas($id)
+    {
+        $cestas = new UserCestasController();
+
+        $cestas->cerrarCesta($id);
+
+        $data = new Request([
+            "usuario_id" => $id,
+            "total" => 0,
+            "estado" => "abierta"
+        ]);
+
+        $cestas->anadirCesta($data);
+    }
+
     public function createPaymentIntent(Request $request)
     {
         Stripe::setApiKey(config('services.stripe.secret'));
 
         try {
+            $usuarioId = $request->input('usuarioId');
+
+            // Validar que usuarioId no es nulo
+            if (is_null($usuarioId)) {
+                return response()->json(['error' => 'El usuarioId es requerido'], 400);
+            }
+
             $paymentIntent = PaymentIntent::create([
                 'amount' => $request->input('amount'),
                 'currency' => 'eur',
                 'payment_method_types' => ['card'],
             ]);
 
+            if (!empty($paymentIntent->client_secret)) {
+                $this->gestionCestas($usuarioId);
+            }
 
             return response()->json([
                 'clientSecret' => $paymentIntent->client_secret,
@@ -28,4 +53,5 @@ class PaymentController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
 }
